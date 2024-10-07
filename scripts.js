acf77d1832bca942932082004bbc3fd457aac58f
JaM0rantHim <ishaans0824@gmail.com>
2024-10-06 21:48:21 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+function changePage(){
+    window.location.href="tickets.html";
+}
+
+function openPopup() {
+    document.getElementById("popupBox").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popupBox").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
+
+function openPopup1() {
+    document.getElementById("popupBox1").style.display = "block";
+    document.getElementById("overlay1").style.display = "block";
+}
+function closePopup1() {
+    document.getElementById("popupBox1").style.display = "none";
+    document.getElementById("overlay1").style.display = "none";
+}
